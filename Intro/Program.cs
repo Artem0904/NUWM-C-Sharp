be786a3b12fb5dbadc6756e9665775c36f2a3e69
Artem0904 <artemshadiuk@gmail.com>
2024-09-23 10:51:18 +0300

    Lab1

--- Intro/Program.cs
+++ Intro/Program.cs
@@ -12,10 +12,24 @@ namespace Intro
         {
             //Console.WriteLine("\tДуми мої, думи мої,\n\n\tЛихо мені з вами!\n\n\tНащо стали на папері\n\n\tСумними рядами?..\n\n\tЧом вас вітер не розвіяв\n\n\tВ степу, як пилину?\n\n\tЧом вас лихо не приспало\n\n");
 
-            Console.Write("Enter your name :  ");
-            string fullName = Console.ReadLine();
-            Console.WriteLine("Your name is " + fullName);
-            
+            //Console.Write("Enter your name :  ");
+            //string name = Console.ReadLine();
+            //Console.Write("Enter your surname :  ");
+            //string surname = Console.ReadLine();
+            //Console.Write("Enter your age :  ");
+            //string age = Console.ReadLine();
+            //Console.WriteLine(name + " " + surname + ", " + age + " years old");
+
+            int d1, d2, sum;
+
+            Console.WriteLine("Enter firs number : ");
+            d1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number : ");
+            d2 = Convert.ToInt32(Console.ReadLine());
+            sum = d1 + d2;
+      
+            Console.WriteLine($"{d1} + {d2} = {sum}");
+
             Console.ReadKey();
         }
     }
---
+++ Lab3/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //#1.2.1 
+
+            //double P, x, y, t;
+            //string buf;
+            //Console.WriteLine("Enter x = ");
+            //buf = Console.ReadLine();
+            //x = Convert.ToDouble(buf);
+            //Console.WriteLine("Enter y = ");
+            //buf = Console.ReadLine();
+            //y = Convert.ToDouble(buf);
+            //Console.WriteLine("Enter t = ");
+            //buf = Console.ReadLine();
+            //t = Convert.ToDouble(buf);
+            //P = (Math.Pow(Math.Sin(x), 3) + Math.Log(2 * y + 3 * x)) / (Math.Pow(t, Math.E) + Math.Sqrt(x));
+            //Console.WriteLine($"x = {x}\n y = {y}\n t = {t}\n P = {P}");
+
+
+            //#1.2.2
+
+            //double y, x, a, b, c = 9;
+            //string buf;
+            //Console.WriteLine("Enter x=");
+            //buf = Console.ReadLine();
+            //x = Convert.ToDouble(buf);
+            //b = x + c * c;
+            //a = Math.Pow(Math.Abs(b + c), 1.0 / 3);
+            //y = Math.Pow(Math.Cos(b), 2) + b * Math.Pow(Math.Cos(a * a), 4);
+            //Console.WriteLine("x = {0} \n a = {1}\n b = {2} \n c ={3} \n y ={4}", x, a, b, c, y);
+
+            //#1.2.3
+
+            //bool A = true, B = false, C = false, D;
+
+            //D = (A | !A & B) | C;
+            //Console.WriteLine("D = ({0} | !{1} & {2}) | {3} = {4}", A, A, B, C, D);
+
+            //D = !A | A & (B | C);
+            //Console.WriteLine("D = !{0} | {1} & ({2} | {3}) = {4}", A, A, B, C, D);
+
+            //D = (A | B & !C) & C;
+            //Console.WriteLine("D = ({0} | {1} & !{2}) & {3} = {4}", A, B, C, C, D);
+
+            //Console.Read();
+
+
+            //Завдання 1
+            //#24
+
+            //Console.Write("Enter a = ");
+            //double a = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Enter x = ");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Enter y = ");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Enter c = ");
+            //double c = Convert.ToDouble(Console.ReadLine());
+
+            //double P = (Math.Pow(a, 5) + Math.Acos(a + Math.Pow(x, 3)) - Math.Pow(Math.Sin(y - c), 4)) /
+            //    (Math.Pow(Math.Sin(x + y), 3) + Math.Abs(x - y));
+
+            //Console.WriteLine($"P = {P}");
+
+
+            //Завдання 2
+            //#8
+
+            //Console.WriteLine("Enter x = ");
+            //double x = Convert.ToDouble(Console.ReadLine());
+            //const double b = 2.2;
+            //const double c = 3.7;
+
+            //double y = (7 * (Math.Pow(Math.E, (Math.Sqrt(Math.Abs(x)))))) + (Math.Pow(Math.Cos(x), 4));
+            //double a = Math.Sin(b);
+            //x = a + Math.Pow((b + c), 3);
+
+            //Console.WriteLine($"y = {y}\n a = {a}\n x = {x}");
+
+            //Завдання 3
+            //#8
+
+            ////A
+            //int x1 = 1;
+            //int y1 = 2;
+            //bool A = (x1 >= 0) && (y1 * 2 != 4);
+            //Console.WriteLine($"A ={A}");
+
+            ////B
+            //int x2 = 2;
+            //int y2 = 1;
+            //bool B = (x2 * y2 != 0) || (y2 > x2);
+            //Console.WriteLine($"B = {B}");
+        }
+    }
+}
