8cb6352178ed92c6f60454e171b604a76f7e31ea
Artem0904 <moonportal708@gmail.com>
2024-11-07 17:10:31 +0200

    Add Lab 2

---
+++ 02_Lab2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            /////1
+            //Console.WriteLine("ПРИВІТ! Вивчаємо мову C#!");
+            //Console.Read();
+
+            //            /////2
+            //            Console.WriteLine(@"Це буквальний літерал, а це -
+            //"" "" - подвійні лапки у ньому;
+            //' ' - одинарні лапки у ньому");
+            //            Console.Read();
+
+            /////3
+            //double f = 1.5F;
+            //char c; 
+            //int i = 0;
+            //bool b = false;
+            //decimal d = 1.5555555555555555555555555M;
+            //double x = Math.Sin(Math.PI / 3);
+            //c = 'A'; 
+            //Console.WriteLine("f = {0} i = {1} x = {2} d = {3} c={4}", f, i, x, d, c);
+            //Console.WriteLine(b.ToString());
+            //Console.Read();
+
+            ////4  
+            //int i = 3;
+            //double y = 4.12;
+            //decimal d = 600m;
+            //string s = "Вася";
+            //Console.WriteLine("i = " + i); 
+            //Console.WriteLine("y = {0} \n d = {1}", y, d);
+            //Console.WriteLine("s = " + s);
+            //Console.Read();
+
+            /////5
+            // ---------------------------------------------
+            Console.WriteLine("Введіть рядок");
+            string s = Console.ReadLine();
+            Console.WriteLine("s = " + s);
+            // ---------------------------------------------
+            Console.WriteLine("Введіть символ");
+            char c = (char)Console.Read();
+            Console.ReadLine();
+            Console.WriteLine("c = " + c);
+            // ---------------------------------------------
+            string buf;
+            Console.WriteLine("Введіть ціле число");
+            buf = Console.ReadLine();
+            int i = Convert.ToInt32(buf);
+            Console.WriteLine(i);
+            // ---------------------------------------------
+            Console.WriteLine("Введіть дійсне число");
+            buf = Console.ReadLine();
+            double x = Convert.ToDouble(buf);
+            Console.WriteLine(x);
+            // ---------------------------------------------
+            Console.WriteLine("Введіть дійсне число");
+            buf = Console.ReadLine();
+            double y = double.Parse(buf);
+            Console.WriteLine(y);
+            // ---------------------------------------------
+            Console.WriteLine("Введіть дійсне число");
+            buf = Console.ReadLine();
+            decimal z = decimal.Parse(buf);
+            Console.WriteLine(z);
+        }
+    }
+}
--- 08_Lab8/Program.cs
+++ 08_Lab8/Program.cs
@@ -4,171 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _08_Lab8
+namespace _05_Lab5
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static double x, y;
+        static double a, b, c;
+        static void Main()
         {
-            Console.OutputEncoding = Encoding.UTF8;
-            ////4.2.1
-            //int[,] Arr = { { 1, 2, 3, 4 }, { 4, 3, 2, 1 }, { 4, 5, 6, 7 } };
-            //Console.WriteLine("Початковий масив :");
+            Console.WriteLine("Введіть значення m:");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введіть значення n:");
+            int n = Convert.ToInt32(Console.ReadLine());
 
-            //for (int i = 0; i < 3; i++)
-            //{
-            //    for (int j = 0; j < 4; j++)
-            //    {
-            //        Console.Write("{0}    ", Arr[i, j]);
-            //    }
-            //    Console.WriteLine();
-            //}
+            int sum = 0;
+            int current = m;
 
-            //for (int i = 0; i < 3; i++)
-            //{
-            //    for (int j = 0; j < 4; j++)
-            //    {
-            //        if (i % 2 == 0 && j % 2 == 0)
-            //        {
-            //            Arr[i, j] = 1;
-            //            for (int k = 1; k <= j; k++) Arr[i, j] *= k;
-            //        }
-            //        else
-            //        {
-            //            Arr[i, j] = 0;
-            //            for (int k = 1; k <= i; k++) Arr[i, j] += k;
-            //        }
-            //    }
-            //}
-            //Console.WriteLine("Оновлений масив :");
-            //for (int i = 0; i < 3; i++)
-            //{
-            //    for (int j = 0; j < 4; j++)
-            //    {
-            //        Console.Write("{0}    ", Arr[i, j]);
-            //    }
-            //    Console.WriteLine();
-            //}
+            do
+            {
+                sum += current * current;
+                current++;
+            } while (current <= n);
 
-            //Random r = new Random(1);
-            //for (int i = 0; i < 3; i++)
-            //{
-            //    for (int j = 0; j < 4; j++)
-            //    {
-            //        Arr[i, j] = r.Next(100);
-            //    }
-            //}
-            //Console.WriteLine("Масив з рандомними значеннями :");
-            //for (int i = 0; i < 3; i++)
-            //{
-            //    for (int j = 0; j < 4; j++)
-            //    {
-            //        Console.Write("{0}    ", Arr[i, j]);
-            //    }
-            //    Console.WriteLine();
-            //}
-            //Console.ReadLine();
-
-            ////4.2.2
-            //int n;
-            //Console.Write("Введіть кількість елементів n: ");
-            //do
-            //{
-            //    n = int.Parse(Console.ReadLine());
-            //    if (n > 0)
-            //        break;
-            //    Console.WriteLine("n має бути > 0");
-            //} while (n < 1);
-
-            //int[,] Arr = new int[n, n];
-            //Random r = new Random(1);
-            //for (int i = 0; i < n; i++)
-            //{
-            //    for (int j = 0; j < n; j++)
-            //    {
-            //        Arr[i, j] = r.Next(100) - r.Next(100);
-            //    }
-            //}
-
-            //Console.WriteLine("Масив Arr:");
-            //for (int i = 0; i < n; i++)
-            //{
-            //    for (int j = 0; j < n; j++)
-            //    {
-            //        Console.Write("{0}    ", Arr[i, j]);
-            //    }
-            //    Console.WriteLine();
-            //}
-
-            //int Sum = 0;
-            //for (int i = 0; i < n; i++)
-            //{
-            //    for (int j = i; j < n; j++)
-            //    {
-            //        if (i < j && Arr[i, j] > 0)
-            //        {
-            //            Sum += Arr[i, j];
-            //        }
-            //    }
-            //}
-            //Console.WriteLine("Сума додатних елементів вище головної діагоналі: {0}", Sum);
-            //Console.ReadLine();
-
-            //4.2.3
-            //int n = 7;
-            //int[,] Arr = new int[n, n];
-            //Random r = new Random(1);
-            //for (int i = 0; i < n; i++)
-            //{
-            //    for (int j = 0; j < n; j++)
-            //    {
-            //        Arr[i, j] = r.Next(100) - r.Next(100);
-            //    }
-            //}
-
-            //Console.WriteLine("Початковий масив:");
-            //for (int i = 0; i < n; i++)
-            //{
-            //    for (int j = 0; j < n; j++)
-            //    {
-            //        Console.Write("{0}\t", Arr[i, j]);
-            //    }
-            //    Console.WriteLine();
-            //}
-
-            //int[] Tmp = new int[7];
-            //for (int i = 0; i < n; i++)
-            //{
-            //    Tmp[i] = 0;
-            //    for (int j = 0; j < n; j++)
-            //    {
-            //        Tmp[i] += Arr[i, j];
-            //    }
-            //}
-
-            //Console.WriteLine("\nСума елементів у кожному рядку:");
-            //for (int i = 0; i < n; i++)
-            //{
-            //    Console.Write("{0}    ", Tmp[i]);
-            //}
-
-            //int Max = Tmp[0];
-            //int NMax = 0;
-            //for (int i = 1; i < n; i++)
-            //{
-            //    if (Max < Tmp[i])
-            //    {
-            //        Max = Tmp[i];
-            //        NMax = i;
-            //    }
-            //}
-            //Console.WriteLine("\nРядок з максимальною сумою: {0} зі значенням {1}", NMax, Max);
-            //Console.WriteLine("Елементи цього рядка:");
-            //for (int j = 0; j < n; j++)
-            //{
-            //    Console.Write("{0}    ", Arr[NMax, j]);
-            //}
-            //Console.ReadLine();
+            Console.WriteLine("Сума квадратів чисел від {0} до {1} дорівнює {2}", m, n, sum);
+            Console.ReadLine();
         }
     }
 }
