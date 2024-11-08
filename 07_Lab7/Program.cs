9f78ebb7ebfb5153570f2628c9227fadd559e591
Artem0904 <artemshadiuk@gmail.com>
2024-11-08 20:09:08 +0200

    Edit Lab 7

--- 07_Lab7/Program.cs
+++ 07_Lab7/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace _07_Lab7
 {
@@ -11,7 +8,8 @@ namespace _07_Lab7
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            /////////////№1 
+
+            /////////////1 
 
             //int n;
             //Console.Write("Введіть кількість елементів n: ");
@@ -144,48 +142,190 @@ namespace _07_Lab7
 
 
             ////////////////4
-            ///
-            int n;
-            Console.Write("Введіть кількість елементів у масиві: ");
-            do
-            {
-                n = int.Parse(Console.ReadLine());
-                if (n > 0)
-                    break;
-                Console.Write("n має бути > 0: ");
-            } while (n < 1);
+            
+            //int n;
+            //Console.Write("Введіть кількість елементів у масиві: ");
+            //do
+            //{
+            //    n = int.Parse(Console.ReadLine());
+            //    if (n > 0)
+            //        break;
+            //    Console.Write("n має бути > 0: ");
+            //} while (n < 1);  
 
-            int[] array = new int[n];
+            //int[] array = new int[n]; 
 
-            Console.WriteLine("Введіть елементи масиву:");
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write($"Ел[{i}] = ");
-                array[i] = int.Parse(Console.ReadLine());
-            }
+            //Console.WriteLine("Введіть елементи масиву:");
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write($"Ел[{i}] = ");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //} 
 
-            int index = 0;
-            for (int i = 0; i < n; i++)
-            {
-                if (array[i] != 0)
-                {
-                    array[index++] = array[i];
-                }
-            }
+            //int index = 0;
+            //for (int i = 0; i < n; i++)
+            //{
+            //    if (array[i] != 0)
+            //    {
+            //        array[index++] = array[i];
+            //    }
+            //} 
 
-            while (index < n)
-            {
-                array[index++] = 0;
-            }
+            //while (index < n)
+            //{
+            //    array[index++] = 0;
+            //} 
 
-            Console.WriteLine("Результат:");
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write(array[i] + " ");
-            }
+            //Console.WriteLine("Результат:");
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write(array[i] + " ");
+            //} 
 
-            Console.WriteLine();
-            Console.ReadKey();
+            //Console.WriteLine();
+            //Console.ReadKey();
+
+
+
+            /////Приклад 3.2.1
+            
+            //string buf;
+            //int Size;
+            //Console.Write("Введіть розмір масиву: ");
+            //buf = Console.ReadLine();
+            //Size = Convert.ToInt32(buf);
+
+            //double[] Arr1 = new double[Size];
+
+            //double x, y;
+            //Console.Write("Введіть x = ");
+            //buf = Console.ReadLine();
+            //x = Convert.ToDouble(buf);
+            //Console.Write("Введіть y = ");
+            //buf = Console.ReadLine();
+            //y = Convert.ToDouble(buf);
+
+            //Arr1[0] = Arr1[1] = x;
+            //Arr1[2] = y;
+
+            //for (int i = 3; i < Arr1.Length; i++) {
+            //    Arr1[i] = Arr1[i - 2] + Arr1[i - 1] * Arr1[i - 3] / Math.Pow(2, i - 1);
+            //}
+
+            //Console.WriteLine("Значення елементів масиву:");
+            //for (int i = 0; i < Size; i++)
+            //    Console.WriteLine("Arr1[{0}] = {1}", i + 1, Arr1[i]);
+
+            //double z;
+            //Console.Write("Введіть z = ");
+            //buf = Console.ReadLine();
+            //z = Convert.ToDouble(buf);
+
+            //int kil = 0;
+            //for (int i = 0; i < Arr1.Length; i++) {
+            //    if (Arr1[i] > z) kil++;
+            //}
+            //Console.WriteLine("\nКількість елементів, більших за {0}: {1}", z, kil);
+            //Console.ReadLine();
+
+
+
+
+	    ////Приклад 3.2.2
+
+            //const int n = 7;
+            //int[] A = new int[n];
+            //int i;
+            //int sum = 0;
+
+            //Console.WriteLine("Введіть елементи масиву A:");
+            //for (i = 0; i < A.Length; i++) {
+            //    Console.Write("A[{0}] = ", i + 1);
+            //    A[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("Введений масив:");
+            //foreach (int k in A)
+            //    Console.Write("{0} ", k);
+            //Console.WriteLine();
+
+            //i = 0;
+            //while (i < n && A[i] >= 0) {
+            //    sum += A[i];
+            //    i++;
+            //}
+            //Console.WriteLine("\nСума додатних елементів = " + sum);
+            //Console.ReadLine();	
+
+
+
+
+	    ////Приклад 3.2.3
+
+	    //const int n = 7;
+            //double[] A = new double[n];
+            //double[] B = new double[n];
+            //int i;
+            //double s = 0;
+
+            //Console.WriteLine("Введіть елементи масиву A:");
+            //for (i = 0; i < A.Length; i++) {
+            //    Console.Write("A[{0}] = ", i + 1);
+            //    A[i] = Convert.ToDouble(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("Введіть елементи масиву B:");
+            //for (i = 0; i < B.Length; i++) {
+            //    Console.Write("B[{0}] = ", i + 1);
+            //    B[i] = Convert.ToDouble(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("Виведення значеннь масиву A:");
+            //foreach(int k in A)
+            //{
+            //    Console.Write("{0}     ", k);
+            //}
+            //Console.WriteLine();
+
+            //Console.WriteLine("Виведення значеннь масиву В");
+            //foreach(int k in B)
+            //{
+            //    Console.Write("{0}     ", k);
+            //}
+            //Console.WriteLine();
+
+            //for (i = 0; i < A.Length; i++)
+            //    s += (A[i] + B[i]) * (A[i] - B[i]);
+            //s *= 2;
+
+            //Console.WriteLine("\nРезультат обчислення s = " + s);
+            //Console.ReadLine();
+
+
+
+ 	        ////Приклад 3.2.4
+
+            //const int n = 7;
+            //double[] A = new double[n];
+            //Random rnd = new Random();
+
+            //for (int i = 0; i < A.Length; i++) {
+            //   A[i] = 2 * rnd.NextDouble() - 1;
+            //}
+            //Console.WriteLine("Масив A до сортування:");
+            //foreach (double k in A)
+            //    Console.Write("{0:f3} ", k);
+            
+            //Array.Sort(A);
+            //Console.WriteLine("\nМасив A після сортування:");
+            //foreach (double k in A)
+            //    Console.Write("{0:f3} ", k);
+
+            //Array.Reverse(A);
+            //Console.WriteLine("\nМасив A у зворотному порядку:");
+            //foreach (double k in A)
+            //    Console.Write("{0:f3} ", k);
+            //Console.ReadLine();
         }
     }
 }
