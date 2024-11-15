8f2881d1c5a04209fb552b6a732261d50c979779
DEV <dev@step.rivne>
2024-11-15 20:38:13 +0200

    Lab 8

--- 08_Lab8/Program.cs
+++ 08_Lab8/Program.cs
@@ -12,22 +12,324 @@ namespace _05_Lab5
         static double a, b, c;
         static void Main()
         {
-            Console.WriteLine("Введіть значення m:");
-            int m = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введіть значення n:");
-            int n = Convert.ToInt32(Console.ReadLine());
+            Console.OutputEncoding = Encoding.UTF8;
 
-            int sum = 0;
-            int current = m;
+            //Console.WriteLine("Введіть значення m:");
+            //int m = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введіть значення n:");
+            //int n = Convert.ToInt32(Console.ReadLine());
 
-            do
-            {
-                sum += current * current;
-                current++;
-            } while (current <= n);
+            //int sum = 0;
+            //int current = m;
 
-            Console.WriteLine("Сума квадратів чисел від {0} до {1} дорівнює {2}", m, n, sum);
-            Console.ReadLine();
+            //do
+            //{
+            //    sum += current * current;
+            //    current++;
+            //} while (current <= n);
+
+            //Console.WriteLine("Сума квадратів чисел від {0} до {1} дорівнює {2}", m, n, sum);
+            //Console.ReadLine();
+
+
+
+
+
+            ///////Приклад 4.2.1
+            //int[,] Arr = {{1,2,3,4},{4,3,2,1},{4,5,6,7}};
+            //Console.WriteLine("Масив Arr з початковими значеннями:");
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < 4; j++)  
+            //    {   
+            //        Console.Write("{0}    ", Arr[i,j]);
+            //    }
+            //    Console.WriteLine();
+            //}
+             
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < 4; j++)  
+            //    { 
+            //        if (i % 2 == 0 && j % 2 == 0)
+            //        { 
+            //            Arr[i, j] = 1;
+            //            for (int k = 1; k <= j; k++) Arr[i, j] *= k;
+            //        }
+            //        else 
+            //        { 
+            //            Arr[i, j] = 0;
+            //            for (int k = 1; k <= i; k++) Arr[i, j] += k;
+            //        }
+            //    }
+            //}
+            //Console.WriteLine("Модифікований масив Arr:");
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < 4; j++)  
+            //    { 
+            //        Console.Write("{0}    ", Arr[i, j]);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //// Заповнення масиву випадковими значеннями
+            //Random r = new Random(1);  
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < 4; j++)  
+            //    { 
+            //       Arr[i, j] = r.Next(100); 
+            //    }
+            //}
+            //Console.WriteLine("Масив Arr з випадковими значеннями:");
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < 4; j++)  
+            //    { 
+            //        Console.Write("{0}    ", Arr[i, j]);
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.ReadLine();
+
+
+
+
+
+            ///////Приклад 4.2.1
+            //int n; // розмірність масиву
+            //Console.WriteLine("Введіть розмірність масиву n:");
+            //n = Convert.ToInt32(Console.ReadLine());
+            //int[,]  Arr = new int[n,n];  
+            //Random r = new Random(1);  
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)  
+            //    { 
+            //        Arr[i, j] = r.Next(100)- r.Next(100);
+            //    }
+            //}
+            //Console.WriteLine("Масив Arr:");
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)  
+            //    {   
+            //        Console.Write("{0}    ", Arr[i,j]);
+            //    }
+            //    Console.WriteLine();
+            //}
+            //int Sum = 0;
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = i; j < n; j++)  
+            //    { 
+            //        if (i < j && Arr[i,j]>0)
+            //        { 
+            //            Sum += Arr[i, j];   
+            //        }
+            //    }
+            //}
+            //Console.WriteLine("Сума додатних елементів вище головної діагоналі: {0}", Sum);
+            //Console.ReadLine();
+
+
+
+
+            ///////Приклад 4.2.3
+            //int n=7; // розмірність масиву
+            //int[,]  Arr = new int[n,n];  
+            //Random r = new Random(1);  
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)  
+            //    { 
+            //        Arr[i, j] = r.Next(100)- r.Next(100);
+            //    }
+            //}
+            //Console.WriteLine("Масив Arr:");
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)  
+            //    {   
+            //        Console.Write("{0}\t", Arr[i,j]);
+            //    }
+            //    Console.WriteLine();
+            //}
+            //int[] Tmp = new int[7];
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Tmp[i] = 0;
+            //    for (int j = 0; j < n; j++)  
+            //    { 
+            //        Tmp[i] += Arr[i, j];
+            //    }
+            //}
+            //Console.WriteLine("\nСума елементів у кожному рядку:");
+            //for (int i = 0; i < n; i++)  
+            //{ 
+            //    Console.Write("{0}    ", Tmp[i]);
+            //}
+            //int Max = Tmp[0];
+            //int NMax = 0;
+            //for (int i = 1; i < n; i++)  
+            //{ 
+            //    if (Max < Tmp[i])
+            //    { 
+            //        Max = Tmp[i];
+            //        NMax = i;
+            //    }
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("Рядок з максимальною сумою: {0} зі значенням {1}", Max, NMax);
+            //Console.WriteLine("Елементи цього рядка:");
+            //for (int j = 0; j < n;j++) 
+            //{ 
+            //    Console.Write("{0}    ", Arr[NMax, j]);
+            //}
+            //Console.ReadLine();
+
+
+
+
+            ///////Завдання 1 11
+            //int N; // Розмірність матриці
+            //Console.WriteLine("Введіть розмірність матриці N:");
+            //N = Convert.ToInt32(Console.ReadLine());
+            
+            //int[,] A = new int[N, N];
+            
+            //// Заповнення матриці згідно з формулою a_ij = i^i - j^j
+            //for (int i = 0; i < N; i++)
+            //{
+            //    for (int j = 0; j < N; j++)
+            //    {
+            //        A[i, j] = (int)Math.Pow(i + 1, i + 1) - (int)Math.Pow(j + 1, j + 1);
+            //    }
+            //}
+            
+            //// Виведення матриці
+            //Console.WriteLine("Матриця A:");
+            //for (int i = 0; i < N; i++)
+            //{
+            //    for (int j = 0; j < N; j++)
+            //    {
+            //        Console.Write("{0}\t", A[i, j]);
+            //    }
+            //    Console.WriteLine();
+            //}
+            
+            //// Обчислення суми додатних елементів
+            //int positiveSum = 0;
+            //for (int i = 0; i < N; i++)
+            //{
+            //    for (int j = 0; j < N; j++)
+            //    {
+            //        if (A[i, j] > 0)
+            //        {
+            //            positiveSum += A[i, j];
+            //        }
+            //    }
+            //}
+            
+            //Console.WriteLine("Сума додатних елементів матриці A: {0}", positiveSum);
+            //Console.ReadLine();
+
+
+
+
+
+
+            ///2   11
+            
+            //int n, m;
+
+            //Console.WriteLine("Введіть кількість рядків n:");
+            //n = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Введіть кількість стовпців m:");
+            //m = Convert.ToInt32(Console.ReadLine());
+
+            //int[,] array = new int[n, m];
+            //Random rand = new Random();
+
+            //// Заповнення масиву випадковими числами і виведення його
+            //Console.WriteLine("Двовимірний масив:");
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < m; j++)
+            //    {
+            //        array[i, j] = rand.Next(1, 101); // Випадкові числа від 1 до 100
+            //        Console.Write(array[i, j] + "\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //int evenCount = 0;
+            //int oddCount = 0;
+
+            //// Підрахунок парних і непарних чисел
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < m; j++)
+            //    {
+            //        if (array[i, j] % 2 == 0)
+            //        {
+            //            evenCount++;
+            //        }
+            //        else
+            //        {
+            //            oddCount++;
+            //        }
+            //    }
+            //}
+
+            //Console.WriteLine("Кількість парних чисел: " + evenCount);
+            //Console.WriteLine("Кількість непарних чисел: " + oddCount);
+            //Console.ReadLine();
+
+
+
+
+
+
+
+
+            //3 11
+
+            //int rows = 7;
+            //int cols = 3;
+            //int[,] matrix = new int[rows, cols];
+            //Random rand = new Random();
+
+            //// Заповнення матриці випадковими числами та виведення її на екран
+            //Console.WriteLine("Матриця 7x3:");
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    for (int j = 0; j < cols; j++)
+            //    {
+            //        matrix[i, j] = rand.Next(1, 101); // Випадкові числа від 1 до 100
+            //        Console.Write(matrix[i, j] + "\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //// Обчислення кількості елементів, кратних 3, для кожного стовпця
+            //for (int j = 0; j < cols; j++)
+            //{
+            //    int count = 0;
+            //    for (int i = 0; i < rows; i++)
+            //    {
+            //        if (matrix[i, j] % 3 == 0)
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //    Console.WriteLine("Кількість елементів у стовпці {0}, які кратні 3: {1}", j + 1, count);
+            //}
+
+            //Console.ReadLine();
         }
     }
 }
