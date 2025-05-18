416826a1d2a5b7a0c533630998c27ed4bb08463d
Artem <artemshadiuk@gmail.com>
2025-05-18 14:27:02 +0200

    Lab 8 9 10

---
+++ Second-semestr/08_Lab8/Program.cs
@@ -0,0 +1,148 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace _7_Lab7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            int left = -10000;
+            int right = 10000;
+            Console.Write("Введіть кількість елементів: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = GenerateRandomArray(size, left, right);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nОригінальний масив:");
+            Console.ForegroundColor = ConsoleColor.White;
+           // PrintArray(array);
+            
+            var watch = Stopwatch.StartNew();
+
+            //int[] sortedArray = InsertionSort(array);
+            int[] sortedArray = QuickSort(array);
+
+            watch.Stop();
+            long sortingTime = watch.ElapsedMilliseconds;
+
+            if (watch.ElapsedMilliseconds == 0)
+            {
+                const int countIterations = 10000;
+                long sum = 0;
+                for (int i = 0; i < countIterations; i++)
+                {
+                    watch.Restart();
+                    //InsertionSort(array);
+                    QuickSort(array);
+                    watch.Stop();
+                    //Console.WriteLine($"\n\nЧас сортування: {watch.ElapsedMilliseconds} мс");
+                    sum += watch.ElapsedMilliseconds;
+                }
+                sortingTime = sum / countIterations;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n\nВідсортований масив: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            PrintArray(sortedArray);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"\n\nЧас сортування: {sortingTime} мс");
+            Console.WriteLine("Кількість порівняннь : " + compareCount);
+            Console.WriteLine("Кількість обмінів : " + moveCount);
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+            Console.ReadKey();
+        }
+        static int[] GenerateRandomArray(int size, int left, int right)
+        {
+            Random rand = new Random();
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+                array[i] = rand.Next(left, right);
+            return array;
+        }
+
+        static void PrintArray(int[] array)
+        {
+            Console.WriteLine(string.Join(", ", array));
+        }
+        static int compareCount = 0;
+        static int moveCount = 0;
+        static int[] InsertionSort(int[] array)
+        {
+            compareCount = 0;
+            moveCount = 0;
+            int[] resultArr = (int[])array.Clone();
+            int n = resultArr.Length;
+            for (int i = 1; i < n; i++)
+            {
+                int key = resultArr[i];
+                int j = i - 1;
+
+                while (j >= 0 && resultArr[j] > key)
+                {
+                    resultArr[j + 1] = resultArr[j];
+                    compareCount++;
+                    j--;
+                }
+                resultArr[j + 1] = key;
+                moveCount++;
+
+                //Console.ForegroundColor = ConsoleColor.Blue;
+                //Console.Write("Після проходу " + (i + 1) + ": ");
+                //Console.ForegroundColor = ConsoleColor.White;
+                //PrintArray(resultArr);
+            }
+            return resultArr;
+        }
+
+        static int[] QuickSort(int[] array)
+        {
+            compareCount = 0;
+            moveCount = 0;
+            int[] result = (int[])array.Clone();
+            QuickSortRecursive(result, 0, result.Length - 1);
+            return result;
+        }
+
+        static void QuickSortRecursive(int[] array, int low, int high)
+        {
+
+            if (low < high)
+            {
+                int pivotIndex = Partition(array, low, high);
+                QuickSortRecursive(array, low, pivotIndex - 1);
+                QuickSortRecursive(array, pivotIndex + 1, high);
+            }
+        }
+
+        static int Partition(int[] array, int low, int high)
+        {
+            int pivot = array[high];
+            int i = low - 1;
+
+            for (int j = low; j < high; j++)
+            {
+                compareCount++;
+                if (array[j] < pivot)
+                {
+                    i++;
+                    (array[i], array[j]) = (array[j], array[i]);
+                    moveCount++;
+                }
+            }
+
+            (array[i + 1], array[high]) = (array[high], array[i + 1]);
+            moveCount++;
+            return i + 1;
+        }
+
+
+    }
+}
---
+++ Second-semestr/09_Lab9-10/Program.cs
@@ -0,0 +1,137 @@
+﻿using System.Text;
+
+namespace _09_Lab9_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("--------- Line ---------");
+            int key = 43;
+            int[] ints = new int[] { 1, 8, 5, 7, 2, 1, 6, 4, 7, 3, 2, 0};
+            Console.Write("Масив : ");
+            PrintArray(ints);
+
+            Console.WriteLine("Ключ : " + key);
+
+            int resultLine = LinSearch(ints, key);
+
+            if (resultLine != -1)
+                Console.WriteLine($"Ключ знайдено на позиції: {resultLine}");
+            else
+                Console.WriteLine("Ключ не знайдено.");
+
+            Console.WriteLine("\n\n--------- KMP ---------");
+            string text = "abcabcabcdabcabcd";   // основний текст
+            string pattern = "abcabcd";          // підрядок для пошуку
+
+            Console.WriteLine("Весь текст : " + text);
+            Console.WriteLine("Підрядок : " + pattern);
+
+            int resultRMP = KMPSearch(text, pattern);
+
+            if (resultRMP != -1)
+                Console.WriteLine($"Підрядок знайдено на позиції: {resultRMP}");
+            else
+                Console.WriteLine("Підрядок не знайдено.");
+        }
+        static int LinSearch(int[] a, int key)
+        {
+            for (int i = 0; i < a.Length; i++)
+                if (a[i] == key)
+                    return i;
+            return -1;
+        }
+        static void PrintArray(int[] array)
+        {
+            Console.WriteLine(string.Join(", ", array));
+        }
+        static int KMPSearch(string text, string pattern)
+        {
+            int n = text.Length;
+            int m = pattern.Length;
+
+            int[] prefix = new int[m];
+            int len = 0;   
+            prefix[0] = 0; 
+
+            for (int i = 1; i < m; i++)
+            {
+                while (len > 0 && pattern[i] != pattern[len])
+                    len = prefix[len - 1];
+
+                if (pattern[i] == pattern[len])
+                    len++;
+
+                prefix[i] = len; 
+            }
+
+            int j = 0;
+
+            for (int i = 0; i < n; i++) 
+            {
+                while (j > 0 && text[i] != pattern[j])
+                    j = prefix[j - 1];
+
+                if (text[i] == pattern[j])
+                    j++;
+
+                if (j == m)
+                    return i - m + 1;
+            }
+
+            return -1;
+        }
+        //static int KMPSearch(string text, string pattern)
+        //{
+        //    int[] prefix = BuildPrefixFunction(pattern);
+        //    int i = 0; // Для text
+        //    int j = 0; // Для pattern
+
+        //    while (i < text.Length)
+        //    {
+        //        if (text[i] == pattern[j])
+        //        {
+        //            i++;
+        //            j++;
+        //        }
+
+        //        if (j == pattern.Length)
+        //            return i - j; // Знайдено
+
+        //        else if (i < text.Length && text[i] != pattern[j])
+        //        {
+        //            if (j != 0)
+        //                j = prefix[j - 1];
+        //            else
+        //                i++;
+        //        }
+        //    }
+
+        //    return -1; // Не знайдено
+        //}
+
+        //// Префікс-функція
+        //static int[] BuildPrefixFunction(string pattern)
+        //{
+        //    int[] prefix = new int[pattern.Length];
+        //    int length = 0;
+        //    prefix[0] = 0;
+
+        //    for (int i = 1; i < pattern.Length; i++)
+        //    {
+        //        while (length > 0 && pattern[i] != pattern[length])
+        //            length = prefix[length - 1];
+
+        //        if (pattern[i] == pattern[length])
+        //            length++;
+
+        //        prefix[i] = length;
+        //    }
+
+        //    return prefix;
+        //}
+    }
+}
